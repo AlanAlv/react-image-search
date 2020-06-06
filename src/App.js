48493900e0fbe71901285cb898892c3ca176b9c3
AlanAlv <alan.alv96@gmail.com>
2020-06-06 04:54:47 -0500

    Pager

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   // App states
   const [ search, saveSearch ] = useState('');
   const [ pics, savePics ] = useState([]);
+  const [ currentPage, saveCurrentPage ] = useState(1);
+  const [ totalPages, saveTotalPages ] = useState(1);
+
 
   useEffect(() => {
     const callAPI = async () => {
@@ -14,32 +17,84 @@ function App() {
 
       const imagesPerPage = 30;
       const key = '16912680-a9141e85075a160802da71bea';
-      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${imagesPerPage}`
+      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page=${imagesPerPage}&page=${currentPage}`
   
       const answer = await fetch(url);
       const result = await answer.json();
 
-      savePics(result.hits);
-  
+      savePics(result.hits);       
+      
+      // Calculate total pages
+      const calculateTotalPages = Math.ceil(result.totalHits / imagesPerPage);
+      saveTotalPages(calculateTotalPages);
+    
+      // Move Screen Up
+      const jumbotron = document.querySelector('.jumbotron');
+      jumbotron.scrollIntoView({ behavior: 'smooth'});
     }
 
     callAPI();
-  }, [search])
+  }, [search, currentPage])
+
+  const previousPage = ()=> {
+    const newCurrentPage = currentPage -1;
+
+    if (newCurrentPage === 0) return;
+
+    saveCurrentPage(newCurrentPage);
+  }
+
+  const nextPage = ()=> {
+    const newCurrentPage = currentPage +1;
+
+    if (newCurrentPage > totalPages ) return;
+
+    saveCurrentPage(newCurrentPage);
+  }
+
+  useEffect (() =>{
+    saveCurrentPage(1);
+  },[search]);
 
   return (
     <div className="container">
       <div className="jumbotron">
+
         <p className="lead text-center">
           Image Search
         </p>
+
         <Form 
           saveSearch={saveSearch}
         />
       </div>
+
       <div className="row justify-content-center">
         <PicsList 
           pics={pics}
         />
+
+        { (currentPage === 1) ? null : (
+          <button
+            type="button"
+            className="btn btn-info mr-2"
+            onClick={previousPage}
+          >
+            &laquo; Previous 
+          </button>
+        )}
+
+        {(currentPage === totalPages) ? null : (
+          <button
+            type="button"
+            className="btn btn-info ml-2"
+            onClick={nextPage}
+          >
+            Next &raquo;
+          </button>
+        )
+
+        }
       </div>
     </div>
   );
--- src/components/PicsList.js
+++ src/components/PicsList.js
@@ -5,8 +5,9 @@ const PicsList = ({pics}) => {
     return ( 
         <div className="col-12 p-5 row">
             {pics.map(pic => (
+                
                 <Pic 
-                    key={pic.key}
+                    key={pic.id}
                     pic={pic}
                 />
             ))}
